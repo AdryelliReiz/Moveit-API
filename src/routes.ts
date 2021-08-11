af2659909e9c44871af2ad1f0dc504eb5ec6f01f
Adryelli <adryellireis0santos@gmail.com>
2021-08-11 19:05:56 -0300

    router update user completed challenges

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { ListUserDataController } from "./controllers/ListUserDataController";
+import { UpdateUserCompletedChallengesController } from "./controllers/UpdateUserCompletedChallengesController";
 import { UpdateUserLevelController } from "./controllers/UpdateUserLevelController";
 import { UpdateUserXPController } from "./controllers/UpdateUserXPController";
 import { ensureAuthenticateUser } from "./middlewares/EnsureAuthenticateUser";
@@ -13,12 +14,15 @@ const authenticateUserController = new AuthenticateUserController();
 const listUserDataController = new ListUserDataController();
 const updateUserXPController = new UpdateUserXPController();
 const updateUserLevelController = new UpdateUserLevelController();
+const updateUserCompletedChallengesController = new UpdateUserCompletedChallengesController();
 
 router.post("/user", createUserController.handle);
 
 router.post("/user/authenticate", authenticateUserController.handle);
 
 router.get("/user", ensureAuthenticateUser, listUserDataController.handle);
+
+router.put("/user/completed-challenges", ensureAuthenticateUser, updateUserCompletedChallengesController.handle);
 
 router.put("/user/xp", ensureAuthenticateUser, updateUserXPController.handle);
 
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { router } from "./routes";
 
 const api = express();
 
+api.use(cors());
 api.use(express.json());
 api.use(router)
 
